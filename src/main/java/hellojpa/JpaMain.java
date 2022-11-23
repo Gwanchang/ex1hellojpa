ce5821e2e7f341f61bd238503bc9f866ca20203a
ChangHyun Kim <copo9760@gmail.com>
2022-11-23 10:56:17 +0900

    jpa

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -121,10 +121,28 @@ public class JpaMain {
             member.getFavoriteFoods().add("족발");
             member.getFavoriteFoods().add("피자");
 
-            member.getAddressHistory().add(new Address("old1", "street1", "1122"));
-            member.getAddressHistory().add(new Address("old2", "street2", "11332"));
+            member.getAddressHistory().add(new AddressEntity("old1", "street1", "1122"));
+            member.getAddressHistory().add(new AddressEntity("old2", "street2", "11332"));
 
             em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            System.out.println("============== START ===============");
+            Member findMember = em.find(Member.class, member.getId());
+
+            //homeCity -> newCity
+            //findMember.getHomeAddress().setCity("newCity"); (X) : 값타입은 통으로 바꿔야 한다.
+            Address a = findMember.getHomeAddress();
+            findMember.setHomeAddress(new Address("newCity", a.getStreet(), a.getZipcode()));
+            
+            //치킨 -> 한식
+            findMember.getFavoriteFoods().remove("치킨");
+            findMember.getFavoriteFoods().add("한식");
+
+            findMember.getAddressHistory().remove(new AddressEntity("old1", "street1", "1122")); //hashcode 중요
+            findMember.getAddressHistory().add(new AddressEntity("newCity1", "street1", "1122"));
 
             tx.commit();
         } catch(Exception e) {
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -34,10 +34,22 @@ public class Member extends BaseEntity{
     @Column(name = "FOOD_NAME")
     private Set<String> favoriteFoods = new HashSet<>();
 
-    @ElementCollection
-    @CollectionTable(name = "ADDRESS", joinColumns =
-        @JoinColumn(name = "MEMBER_ID"))
-    private List<Address> addressHistory = new ArrayList<>();
+//    @ElementCollection
+//    @CollectionTable(name = "ADDRESS", joinColumns =
+//        @JoinColumn(name = "MEMBER_ID"))
+//    private List<Address> addressHistory = new ArrayList<>();
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "MEMBER_ID")
+    private List<AddressEntity> addressHistory = new ArrayList<>();
+
+    public List<AddressEntity> getAddressHistory() {
+        return addressHistory;
+    }
+
+    public void setAddressHistory(List<AddressEntity> addressHistory) {
+        this.addressHistory = addressHistory;
+    }
 
     public Address getHomeAddress() {
         return homeAddress;
@@ -55,13 +67,7 @@ public class Member extends BaseEntity{
         this.favoriteFoods = favoriteFoods;
     }
 
-    public List<Address> getAddressHistory() {
-        return addressHistory;
-    }
 
-    public void setAddressHistory(List<Address> addressHistory) {
-        this.addressHistory = addressHistory;
-    }
 
     public Long getId() {
         return id;
